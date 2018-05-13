44f95de80a531f0a782bd2b4d108728e4c744bc3
ProTip <greg.zapp@gmail.com>
2018-05-12 21:36:50 -0500

    Integrate with Travis-CI

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,33 @@
 import * as Path from 'path'
 
+import Axios from 'axios'
 import chalk, { Chalk } from 'chalk'
 import indent from 'indent-string'
 
 import {exec} from './async/child-process'
+import {EnvBuilder} from './env-builder'
+import {Client} from './rundeck-client/client'
 import {TestRepo} from './test-repo'
 
-import Axios, { AxiosError } from 'axios';
-import {Client} from './rundeck-client/client'
+const DEFAULT_RD_URL = 'http://localhost:8080'
+const DEFAULT_RD_USER = 'admin'
+const DEFAULT_RD_PASSWORD = 'admin'
 
 async function asyncMain() {
-    process.env.RD_URL = 'http://localhost:8080'
-    process.env.RD_USER = 'admin'
-    process.env.RD_PASSWORD = 'admin'
+    process.env.RD_URL = DEFAULT_RD_URL
+    process.env.RD_USER = DEFAULT_RD_USER
+    process.env.RD_PASSWORD = DEFAULT_RD_PASSWORD
+
+    const client = new Client({apiUrl: DEFAULT_RD_URL, username: DEFAULT_RD_USER, password: DEFAULT_RD_PASSWORD})
+
+    const envBuilder = new EnvBuilder(client)
+
+    console.log(`
+Setting up test environment.
+This can take a few minutes if the docker conatiner is not running.
+Sit tight...\n
+    `)
+    await envBuilder.up()
 
     const testRepo = await TestRepo.CreateTestRepo('./tests')
 
